95916562b4abc03f35f12f3704a1e7aa0e771a38
emon535@gmail.com <emon535@gmail.com>
2019-10-10 19:35:56 +0600

    Exercise 2_18 WIP

---
+++ PMSCS/Assignment/Chapter2/Window.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+class Window extends JFrame {
+    // JFrame helloWindow = new JFrame();
+    JTextField nameField = new JTextField(30);
+    JLabel nameLabel = new JLabel();
+
+    public NameWindow()
+    {
+        setSize(800, 600);
+        setTitle("Welcome to java");
+
+        // helloWindow.add(nameField);
+        // helloWindow.add(nameLabel);
+        setVisible(true);
+    }
+
+    }
+
+    public static void main(String[] args) {
+        NameWindow nameWindow = new NameWindow();
+        System.out.println("I m happy with the first code run!");
+
+    }
