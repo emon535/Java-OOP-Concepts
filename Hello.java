b3cd96f06747d99e0b6a0fcf24031cbf0960fb49
emon535@gmail.com <emon535@gmail.com>
2019-10-10 19:09:59 +0600

    Exercise 2_17 Done

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+// Exercise no: Chapter2_17
+
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("I m happy with the first code run!");
+
+    }
+}
---
+++ PMSCS/Assignment/Chapter2/Exercise2_17.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+// Exercise no: Chapter2_17
+
+public class Exercise2_17 {
+    public static void main(String[] args) {
+
+        JFrame helloWindow = new JFrame();
+        helloWindow.setSize(800, 600);
+        helloWindow.setTitle("Welcome to java");
+        helloWindow.setVisible(true);
+
+        System.out.println("I m happy with the first code run!");
+
+    }
+}
